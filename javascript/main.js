31439a2ab5ee53673bed9fd32f6d81799021f568
lushain <vyom262327@gmail.com>
2021-12-20 10:18:23 +0530

    works

--- javascript/main.js
+++ javascript/main.js
@@ -10,25 +10,24 @@ Array.from(elems).forEach(element => {
   element.onclick = function() {scroll(element)};
 });
 
-window.addEventListener('scroll',function(){
-  let offset = window.pageYOffset;
-  const navBar = document.getElementById("nav-bar");
-  if (offset < 769) {
-    navBar.style.backgroundColor = "#212121";
+const options = {
+  root: null,
+  threshold: 0.6
+}
 
-  } else if (769 <= offset && offset < 1538) {
-    navBar.style.backgroundColor = "#0040ff";
+const sections = document.getElementsByTagName('section');
+const navbar = document.getElementById('nav-bar');
 
-  } else if (1538 <= offset && offset < 2307) {
-    navBar.style.backgroundColor = "#b57c00";
+const observer = new IntersectionObserver(function(entries,observer){
+  entries.forEach(entry => {
+    if (entry.isIntersecting){
+      let colour = window.getComputedStyle(entry.target).backgroundColor
+      // let colour = entry.target.style.backgroundColor
+      navbar.style.backgroundColor = colour;
+    }
+  });
+},options);
 
-  } else if (2307 <= offset && offset < 3076) {
-    navBar.style.backgroundColor = "#10a100";
-
-  } else if (3076 <= offset && offset < 3845) {
-    navBar.style.backgroundColor = "#8c1212";
-
-  } else if (3845 <= offset && offset < 4614) {
-    navBar.style.backgroundColor = "#636363";
-  }
+Array.from(sections).forEach(section => {
+  observer.observe(section);
 });
