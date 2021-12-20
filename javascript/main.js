8ec7dee05d56393e328090dfe1fbdef5c7a4c353
lushain <vyom262327@gmail.com>
2021-12-20 23:12:45 +0530

    works with js

--- javascript/main.js
+++ javascript/main.js
@@ -9,11 +9,18 @@ function scroll(element) {
 }
 
 function mouseover(element){
-  element.style.padding = "2vh 9px"
+  element.style.padding = "8vh 3vw"
   element.style.margin = "5vh 0px 5vh 10px"
   let sub = element.firstElementChild
-  console.log(sub);
   sub.style.display = "block"
+}
+function mouseout(element){
+  element.style.width = "0px"
+  element.style.height = "0px"
+  element.style.padding = "4vh 9px"
+  element.style.margin = "8vh 5px 8vh 0px"
+  let sub = element.firstElementChild
+  sub.style.display = "none"
 }
 
 const options = {
@@ -39,6 +46,7 @@ Array.from(elems).forEach(element => {
   element.onclick = function() {scroll(element)};
 });
 
-// Array.from(elems).forEach(element => {
-//   element.onmouseover = function() {mouseover(element)};
-// });
+Array.from(elems).forEach(element => {
+  element.onmouseover = function() {mouseover(element)};
+  element.onmouseout = function() {mouseout(element)};
+});
