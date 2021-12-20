afc8bda295452036c4a82f147702312ca66cee46
lushain <vyom262327@gmail.com>
2021-12-20 20:53:01 +0530

    works

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,25 @@
+const elems = document.getElementsByClassName("nav-icon");
+const sections = document.getElementsByTagName('section');
+const navbar = document.getElementById('nav-bar');
+
 function scroll(element) {
   var temp = element.id
-  temp = temp.slice(0, -5);
-  document.getElementById(temp).scrollIntoView({behavior: 'smooth'});
+  temp = temp.slice(0, -5)
+  document.getElementById(temp).scrollIntoView({behavior: 'smooth', block:'center'});
 }
 
-const elems = document.getElementsByClassName("nav-icon");
-
-Array.from(elems).forEach(element => {
-  element.onclick = function() {scroll(element)};
-});
+function mouseover(element){
+  element.style.padding = "2vh 9px"
+  element.style.margin = "5vh 0px 5vh 10px"
+  let sub = element.firstElementChild
+  console.log(sub);
+  sub.style.display = "block"
+}
 
 const options = {
   root: null,
-  threshold: 0.6
+  threshold: 0.2
 }
-
-const sections = document.getElementsByTagName('section');
-const navbar = document.getElementById('nav-bar');
 
 const observer = new IntersectionObserver(function(entries,observer){
   entries.forEach(entry => {
@@ -31,3 +34,11 @@ const observer = new IntersectionObserver(function(entries,observer){
 Array.from(sections).forEach(section => {
   observer.observe(section);
 });
+
+Array.from(elems).forEach(element => {
+  element.onclick = function() {scroll(element)};
+});
+
+// Array.from(elems).forEach(element => {
+//   element.onmouseover = function() {mouseover(element)};
+// });
